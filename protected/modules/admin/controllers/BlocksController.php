b7ca9746becb2a80bd10af4f1c96ca8ee6c459ae
Alex V <darkoffalex@yandex.ru>
2015-06-17 14:43:33 +0300

    Working on blocks listing and filtering...

--- protected/modules/admin/controllers/BlocksController.php
+++ protected/modules/admin/controllers/BlocksController.php
@@ -2,5 +2,57 @@
 
 class BlocksController extends ControllerAdmin
 {
+    /**
+     * Entry point
+     */
+    public function actionIndex()
+    {
+        $this->redirect(Yii::app()->createUrl('admin/blocks/list'));
+    }
 
+
+    /**
+     * Listing all content items
+     * @param int $tid
+     * @param int $cid
+     * @param int $page
+     */
+    public function actionList($tid = 0, $cid = 0, $page = 1)
+    {
+        //all content types
+        $contentTypes = ContentTypeEx::model()->findAll();
+        //all categories (assoc array for drop-downs)
+        $categoriesList = TreeEx::model()->listAllItemsForForms(0,'-');
+        //selected type
+        $selectedType = ContentTypeEx::model()->findByPk((int)$tid);
+        //selected category
+        $selectedCategory = TreeEx::model()->findByPk((int)$cid);
+
+        //empty conditions
+        $conditions = array();
+
+        //add required conditions
+        if(!empty($selectedType)){
+            $conditions['content_type_id'] = $selectedType->id;
+        }
+        if(!empty($selectedCategory)){
+            $conditions['tree_id'] = $selectedCategory->id;
+        }
+
+        //find all content blocks by conditions
+        $blocks = ContentItemEx::model()->findAllByAttributes($conditions,array('order' => 'priority ASC'));
+
+        //paginate items
+        $perPage = getif($this->global_settings->per_page_qnt,10);
+        $items = CPager::getInstance($blocks,$perPage,$page)->getPreparedArray();
+
+        //render list
+        $this->render('list',array(
+                'items' => $items,
+                'types' => $contentTypes,
+                'categories' => $categoriesList,
+                'selectedType' => $selectedType,
+                'selectedCategory' => $selectedCategory
+            ));
+    }
 }
--- protected/modules/admin/views/blocks/list.php
+++ protected/modules/admin/views/blocks/list.php
@@ -1,39 +1,65 @@
 <?php /* @var $items ContentItemEx[] */ ?>
-<?php /* @var $category TreeEx[] */ ?>
+<?php /* @var $types ContentTypeEx[] */ ?>
+<?php /* @var $categories array */ ?>
+<?php /* @var $selectedType ContentTypeEx */ ?>
+<?php /* @var $selectedCategory TreeEx */ ?>
 
 <main>
     <div class="title-bar">
         <h1><?php echo __a('Content blocks'); ?></h1>
         <ul class="actions">
+            <select name="category" class="float-left filter-drop-down">
+                <option value="1">Category 1</option>
+                <option value="2">Category 2</option>
+<!--                --><?php //foreach($categories as $id => $label): ?>
+<!--                    <option value="--><?php //echo $id ?><!--">--><?php //echo $label; ?><!--</option>-->
+<!--                --><?php //endforeach;?>
+            </select>
             <li><a href="" class="action add"></a></li>
-            <li><a href="" class="action del delete-all-pages" data-id="checkbox"></a></li>
         </ul>
     </div><!--/title-bar-->
-    <div class="content list">
-        <div class="list-row title">
-            <div class="cell checkbox"><input type="checkbox" value="0" name="checkbox" id="checkall_pages"/></div>
-            <div class="cell"><?php echo __a('Label'); ?></div>
-            <div class="cell action"><?php echo __a('Actions'); ?></div>
-        </div><!--/list-row-->
 
-        <div class="list-row">
-            <?php foreach($items as $item): ?>
-                <div class="cell checkbox"><input type="checkbox" name="del_block[<?php echo $item->id; ?>]" value="1"/></div>
-                <div class="cell"><?php echo $item->label; ?></div>
-                <div class="cell action">
-                    <a href="<?php echo Yii::app()->createUrl('admin/blocks/edit',array('id' => $item->id)); ?>" class="action edit edit-page"></a>
-                    <a href="<?php echo Yii::app()->createUrl('admin/blocks/delete',array('id' => $item->id)); ?>" class="action delete delete-page confirm-box"></a>
-                </div>
-            <?php endforeach;?>
-        </div><!--/list-row-->
+    <?php if(!empty($items)): ?>
+        <div class="content list">
+            <div class="list-row title">
+                <div class="cell"><?php echo __a('Label'); ?></div>
+                <div class="cell type"><?php echo __('Type'); ?></div>
+                <div class="cell type"><?php echo __('Category'); ?></div>
+                <div class="cell action"><?php echo __a('Actions'); ?></div>
+            </div><!--/list-row-->
 
-    </div><!--/content-->
+            <div class="list-row">
+                <?php foreach($items as $item): ?>
+                    <div class="cell"><?php echo $item->label; ?></div>
+                    <div class="cell type"><?php echo $item->contentType->label; ?></div>
+                    <div class="cell type"><?php echo $item->tree->label; ?></div>
+                    <div class="cell action">
+                        <a href="<?php echo Yii::app()->createUrl('admin/blocks/edit',array('id' => $item->id)); ?>" class="action edit edit-page"></a>
+                        <a href="<?php echo Yii::app()->createUrl('admin/blocks/delete',array('id' => $item->id)); ?>" class="action delete delete-page confirm-box"></a>
+                    </div>
+                <?php endforeach;?>
+            </div><!--/list-row-->
+        </div><!--/content-->
 
-    <?php if(C) ?>
-    <div class="pagination">
-        <a href="pages.html" class="active">1</a>
-        <a href="pages.html">2</a>
-        <a href="pages.html">3</a>
-        <a href="pages.html">4</a>
-    </div><!--/pagination-->
+        <?php if(CPager::getInstance()->getTotalPages() > 1): ?>
+            <div class="pagination">
+                <?php $controller = Yii::app()->controller->id; ?>
+                <?php $action = Yii::app()->controller->action->id; ?>
+                <?php $cid = Yii::app()->request->getParam('cid',0); ?>
+                <?php $tid = Yii::app()->request->getParam('tid',0); ?>
+                <?php for($i=0; $i < CPager::getInstance()->getTotalPages(); $i++):
+                    $url = Yii::app()->createUrl('admin/'.$controller.'/'.$action,array('page' => $i+1, 'cid' => $cid, 'tid' => $tid));
+                    $active = CPager::getInstance()->getCurrentPage() == $i+1;
+                    ?>
+                    <a href="<?php echo $url; ?>" <?php if($active): ?>class="active"<?php endif;?>><?php echo $i+1; ?></a>
+                <?php endfor;?>
+            </div><!--/pagination-->
+        <?php endif;?>
+    <?php else: ?>
+        <div class="content list">
+            <div class="list-row">
+                <div class="cell"><?php echo __('List is empty'); ?></div>
+            </div><!--/list-row-->
+        </div>
+    <?php endif;?>
 </main>
